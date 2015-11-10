5f6329b6b9ca00d00584d339ad0445dc2e66f3f2
Yitong Ding <404776130@qq.com>
2015-11-09 21:35:32 -0600

    21:35

--- Benchmark.c
+++ Benchmark.c
@@ -127,69 +127,22 @@ print_res(void) {
 /******************************************************/
 /******************************************************/
 
-double fabs(double n)
-{
-  double f;
-
-  if (n >= 0) f = n;
-  else f = -n;
-  return f;
-}
-
-float sqrt(float val)
-{
-  float x = val/10;
-
-  float dx;
-
-  double diff;
-  double min_tol = 0.00001;
-
-  int i, flag;
-
-  flag = 0;
-  if (val == 0 ) x = 0;
-  else {
-    for (i=1;i<20;i++)
-      {
-	if (!flag) {
-	  dx = (val - (x*x)) / (2.0 * x);
-	  x = x + dx;
-	  diff = val - (x*x);
-	  if (fabs(diff) <= min_tol) flag = 1;
-	}
-	else 
-	  x =x;
-      }
-  }
-  return (x);
-}
-
-static inline void
-workload()
-{
-  float x=1234;
-  sqrt(x);
-  //return 0;
-}
-
-
 
 /******************************************************/
 /******************************************************/
 /******************************************************/
 /******************************************************/
-/*
+
 static inline void
 workload()
 {
     double temp = 0;
     long long i = 0;
 
-    for (i = 0; i < 24000; i++)
+    for (i = 0; i < 2000; i++)
     temp = sqrt((double)i*i);
 }
-*/
+
 
 /*
  * each job's work
